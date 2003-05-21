59d0f5c1d5af6872ae72e520a77836b2125700df
bharring <bharring>
2003-05-21 05:39:14 +0000

    *** empty log message ***

--- tar.c
+++ tar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int verify_str_chksum(const char *entry);
 struct tar_entry *convert_str_tar_entry(char *block);
 
 struct tar_entry {
@@ -10,7 +11,7 @@ struct tar_entry {
     unsigned long       size;
     /*unsigned long     mtime;*/
     unsigned char       *mtime[12];
-    unsigned int        chksum;
+    unsigned long       chksum;
     unsigned char       typeflag;
     unsigned char       *linkname[100];
     unsigned char       *magic[6];
@@ -28,15 +29,48 @@ struct tar_entry {
     
 };
 
+int verify_str_chksum(const char *block)
+{
+    unsigned long chksum=0;
+    unsigned long desired_chksum=0;
+    unsigned char tmp8[9];
+    unsigned int x;
+    //printf("passed block='%.512s'\n", (char *)block);
+    /* grab the chksum from the block. */
+    strncpy((char *)&tmp8, (const char*)(block+148), 8);
+    tmp8[8]='\0';
+    desired_chksum=(unsigned long)strtol((const char *)tmp8, NULL, 8);
+    //printf("desired chksum(%lu): ", desired_chksum);
+    for(x=0; x< 148; x++) {
+        //printf("%.5s", entry[x]);
+        //printf("%c",(char)entry[x]);
+        chksum += (unsigned char)block[x];
+    }
+    //chksum'ing treats the stored chksum as 8 blank chars.
+    chksum+=256;
+    //skip 12 bytes at 148, cause the chksum is there, and it's to be treated as 0's...
+    for(x=156; x< 512; x++)
+        chksum += (unsigned char)block[x];
+    //fprintf(stderr, "computed('%lu')\n", chksum);
+    return (desired_chksum != chksum );
+}
+
 struct tar_entry *convert_str_tar_entry(char *block/*[512]*/)
 {
     char tmp8[9];
     char tmp12[13];
     struct tar_entry *t;
+    unsigned long chksum;
     /* = (struct tar_entry *)malloc(sizeof(struct tar_entry)); */
     //t.name = *char[100];
     /* find out why the fuck it's bitching about god damned type conversions when the type is right.*/
     /*That Is Fucking Annoying.*/
+    if (verify_str_chksum(block)!=0) {
+        fprintf(stderr, "shite chksum didn't match on tar header\n");
+        //fprintf(stderr, "correct chksum('%lu') != computed chksum('%lu')\n", t->chksum, chksum);
+        //fprintf(stderr, "shite, chksum didn't match for block, name('%.100s')\n", t->name);
+        exit(EXIT_FAILURE);
+    }
     if((t=(struct tar_entry *)malloc(sizeof(struct tar_entry)))==NULL){
         printf("Shite.  Couldn't allocate needed memory...\n");
         exit(1);
@@ -51,12 +85,12 @@ struct tar_entry *convert_str_tar_entry(char *block/*[512]*/)
     strncpy((char *)tmp8, (const char *)block, 8);          tmp8[8]='\0';
     t->gid = strtol((char *)tmp8, NULL,8);                  block+=8;
     strncpy((char *)tmp12, (const char *)block,12);         tmp12[12]='\0';
-    t->size = strtol((char *)tmp12, NULL,8);               block+=12;
+    t->size = (unsigned long)strtol((char *)tmp12, NULL,8);               block+=12;
     /*strncpy((char *)tmp12, (const char *)block, 12);        tmp12[12]='\0';
     t->mtime = strtol((char *)tmp12, NULL, 12);             block+=12;*/
     strncpy((char *)t->mtime, (const char *)block, 12);      block+=12;
     strncpy((char *)tmp8, (const char *)block, 8);          tmp12[8]='\0';
-    t->chksum = strtol((char *)tmp8, NULL,8);               block+=8;
+    t->chksum = (unsigned long)strtol((char *)tmp8, NULL,8);               block+=8;
     t->typeflag = *block;                                   block++;
     strncpy((char *)t->linkname, (const char *)block,100);   block+=100;
     strncpy((char *)t->magic, (const char *)block, 6);       block+=6;
