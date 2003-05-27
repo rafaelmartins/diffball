0640827e1368a94af478ca5a87ea441128f30ecd
bharring <bharring>
2003-05-27 08:04:53 +0000

    *** empty log message ***

---
+++ adler32.c
@@ -0,0 +1,53 @@
+/*as per the norm, like everyone else this is taken from the zlib package, 
+  with modifications to integrate it/eliminate some of the zlib-isms.
+  Problems with this implementation are my screw-up, not the zlib crew.  Bug me, not them. */
+
+/* adler32.c -- compute the Adler-32 checksum of a data stream
+ * Copyright (C) 1995-2002 Mark Adler
+ * For conditions of distribution and use, see copyright notice in zlib.h 
+ */
+
+/* @(#) $Id: adler32.c,v 1.1 2003/05/27 08:04:53 bharring Exp $ */
+
+//#include "zlib.h"
+#include <stdlib.h>
+
+#define BASE 65521L /* largest prime smaller than 65536 */
+#define NMAX 5552
+/* NMAX is the largest n such that 255n(n+1)/2 + (n+1)(BASE-1) <= 2^32-1 */
+
+#define DO1(buf,i)  {s1 += buf[i]; s2 += s1;}
+#define DO2(buf,i)  DO1(buf,i); DO1(buf,i+1);
+#define DO4(buf,i)  DO2(buf,i); DO2(buf,i+2);
+#define DO8(buf,i)  DO4(buf,i); DO4(buf,i+4);
+#define DO16(buf)   DO8(buf,0); DO8(buf,8);
+
+/* ========================================================================= */
+unsigned long adler32(unsigned long adler, unsigned char *buf, unsigned int len)
+    /*unsigned long adler;
+    unsigned char *buf;
+    unsigned int len;*/
+{
+    unsigned long s1 = adler & 0xffff;
+    unsigned long s2 = (adler >> 16) & 0xffff;
+    int k;
+
+    if (buf == NULL) return 1L;
+
+    while (len > 0) {
+        k = len < NMAX ? len : NMAX;
+        len -= k;
+        while (k >= 16) {
+            DO16(buf);
+	    buf += 16;
+            k -= 16;
+        }
+        if (k != 0) do {
+            s1 += *buf++;
+	    s2 += s1;
+        } while (--k);
+        s1 %= BASE;
+        s2 %= BASE;
+    }
+    return (s2 << 16) | s1;
+}
---
+++ differ.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+//#include "adler32.h"
+
+int main(int argc, char **argv)
+{
+    unsigned long int x;
+    uLong y;
+    int src_fh, trg_fh, out_fh;
+    if(argc <3){
+	printf("pardon, but...\nI need at least 2 args- (source file), (target file), [patch-file]\n");
+	exit(EXIT_FAILURE);
+    }
+    if ((src_fh = open(argv[1], O_RDONLY,0)) == -1) {
+	printf("Couldn't open %s, does it exist?\n", argv[1]);
+	exit(EXIT_FAILURE);
+    }
+    if ((trg_fh = open(argv[2], O_RDONLY,0)) == -1) {
+	printf("Couldn't open %s, does it exist?\n", argv[2]);
+	exit(EXIT_FAILURE);
+    }
+    if(argc==3) {
+	if((out_fh = dup(0))==-1){
+	    printf("well crud, couldn't duplicate stdout.  Likely a bug in differ.\n");
+	    exit(EXIT_FAILURE);
+	}
+    } else if((out_fh = open(argv[3], O_RDWR | O_TRUNC | O_CREAT,0))==-1) {
+	printf("Couldn't create\truncate patch file.\n");
+	exit(EXIT_FAILURE);
+    } else {
+	fprintf(stderr,"storing generated delta in '%s'\n", argv[3]);
+    }
+    
+}
