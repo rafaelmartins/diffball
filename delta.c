4669acf2f2c97741769ddf486382e3da645592a6
bharring <bharring>
2003-06-06 07:32:38 +0000

    adding lookback buffer, likely major clean-up/refactoring to come after. getting a bit messy.  also added update-cvs, simple bash script for commiting ONLY the files listed within.  since my working dir is a bit messy, this saves on cvs headaches.

--- delta.c
+++ delta.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include "adler32.h"
+#include "delta.h"
 /* this is largely based on the algorithms detailed in randal burn's various papers.
    Obviously credit for the alg's go to him, although I'm the one who gets the dubious
    credit for bugs in the implementation of said algorithms... */
+#define LOOKBACK_SIZE 100000
+
+inline void dc_add(struct deltaCommandLoc
+
 
 inline unsigned long hash_it(unsigned long chk, unsigned long tbl_size)
 {
@@ -18,7 +23,8 @@ char *OneHalfPassCorrecting(unsigned char *ref, unsigned long ref_len,
     unsigned long x, index, len;
     unsigned long s1, s2;
     unsigned long empties=0, good_collisions=0, bad_collisions=0;
-    unsigned char *vc, *va, *vs, *sp;
+    unsigned char *vc, *va, *vs; //va=adler start, vs=first non-encoded byte.
+    struct deltaCommandLoc *dc_buffer, *dc_head, *dc_tail, *dc_buffer_end;
     s1=s2=0;
     if((hr=(unsigned long*)malloc(sizeof(unsigned long)*(ref_len - seed_len)))==NULL) {
 	perror("Shite.  couldn't allocate needed memory for reference hash table.\n");
@@ -61,6 +67,11 @@ char *OneHalfPassCorrecting(unsigned char *ref, unsigned long ref_len,
 	    printf("hr[%u]='%lu'\n", x, hr[x]);
     }*/
     printf("version run:\n");
+    printf("creating lookback buffer\n");
+    if((dc_buffer=(struct deltaCommandLoc *)malloc(sizeof(struct deltaCommandLoc)*LOOKBACK_SIZE))==NULL){
+	perror("Shite.  couldn't allocate needed memory for lookback block.\n");
+	exit(EXIT_FAILURE);
+    }
     good_collisions = bad_collisions =0;
     vs = vc = (unsigned char*)ver;
     va=NULL; //this is the starting pt of the adler chksum of len(seed_len).
@@ -106,6 +117,14 @@ char *OneHalfPassCorrecting(unsigned char *ref, unsigned long ref_len,
 	    }
 	    printf("vstart(%lu), rstart(%lu), len(%lu)\n", (unsigned char*)vc -x - (unsigned char*)ver,
 		hr[index] - x, len);
+	    if (vs <= vm) {
+		if (vs!
+	    } else if (vm < vs) {
+		
+	    } else {
+		printf("what in the fuck... hit 3rd conditional on correction.  this means what?\n");
+		exit(EXIT_FAILURE);
+	    }
 	    vc += len;
 	    continue;
 	}
--- delta.h
+++ delta.h
@@ -1,3 +1,11 @@
+#ifndef _DELTA_HEADER
+#define _DELTA_HEADER 1
 char *OneHalfPassCorrecting(unsigned char *ref, unsigned long ref_len,
     unsigned char *ver, unsigned long ver_len, unsigned int seed_len);
 
+struct deltaCommandLoc {
+    unsigned long offset;
+    unsigned char len;
+};
+
+#endif
