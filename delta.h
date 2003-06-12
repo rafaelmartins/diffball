46bbbf2b8196bcc0c6376dfe947ae58e5c62cf2a
bharring <bharring>
2003-06-12 06:40:31 +0000

    for statistical info. try diffing/patching evolution.  It fails the md5summing, and I want to know why.

--- delta.h
+++ delta.h
@@ -20,4 +20,21 @@ struct CommandBuffer {
     struct DCLoc *lb_start, *lb_end, *lb_head, *lb_tail;
 };
 
+struct DCStats {
+    unsigned long copy_count;
+    unsigned long add_count;
+    unsigned long offset_max;
+    unsigned long offset_min;
+    unsigned long add_len_max;
+    unsigned long add_len_min;
+    unsigned long copy_len_max;
+    unsigned long copy_len_min;
+    signed long   neg_offset_max;
+    signed long   neg_offset_min;
+    signed long   pos_offset_max;
+    signed long   pos_offset_min;
+    unsigned long adjacent_offset_max;
+    unsigned long adjacent_offset_min;
+};
+
 #endif
