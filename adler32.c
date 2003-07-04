63282197701f00be6c435b364a686b6430981d15
bharring <bharring>
2003-07-04 07:06:43 +0000

    added gpl licensing header to *.{c,h}

--- adler32.c
+++ adler32.c
@@ -1,10 +1,27 @@
+/*
+  Copyright (C) 2003 Brian Harring
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US 
+*/
 /* as per the norm, modified version of zlib's adler32. */
 /* adler32.c -- compute the Adler-32 checksum of a data stream
  * Copyright (C) 1995-2002 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
-/* @(#) $Id: adler32.c,v 1.3 2003/05/29 07:17:01 bharring Exp $ */
+/* @(#) $Id: adler32.c,v 1.4 2003/07/04 07:06:43 bharring Exp $ */
 
 //#include "zlib.h"
 
