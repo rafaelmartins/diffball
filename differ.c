44d36e29dbad2d65fdea5d007d84cc0a18fe7588
bharring <bharring>
2003-07-09 07:41:16 +0000

    prepping to use autoconf and automake, about there.

--- differ.c
+++ differ.c
@@ -24,7 +24,7 @@
 #include <unistd.h>
 #include "cfile.h"
 #include "diff-algs.h"
-#include "bit-functions.h"
+//#include "bit-functions.h"
 #include <string.h>
 
 unsigned long convertDec(unsigned char *buff, unsigned int len)
