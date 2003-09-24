5ec54c96f4d994cdd2cec4e0bf0d3955e4fba4f5
bharring <bharring>
2003-09-24 09:14:38 +0000

    Fixed a dumb mistake, dawned on me while attempting to sleep- basically the macro definitions (FORMAT_OPTIONS for instance) should have they're 'variables' wrapped in ()'s, since the dev might pass a statement like buff +3 , which should be interpreted as (buff + 3) where ever used. Dumb mistake, fixed though.

--- options.h
+++ options.h
@@ -22,21 +22,21 @@
 enum {OVERSION=100, OVERBOSE,OFORMAT,OSEED,OSAMPLE,OSTDOUT,OBZIP2,OGZIP};
 /*lname, sname, info, ptr, val, desc, args */
 #define DIFF_OPTIONS(seed_len, sample_rate, hash_size)		\
-{"seed-len",	'b', POPT_ARG_INT, &seed_len,0,0,0},		\
-{"sample-rate",	's', POPT_ARG_LONG, &sample_rate,0,0,0},	\
-{"hash-size",	'a', POPT_ARG_LONG, &hash_size,0,0,0}
+{"seed-len",	'b', POPT_ARG_INT, &(seed_len),0,0,0},		\
+{"sample-rate",	's', POPT_ARG_LONG, &(sample_rate),0,0,0},	\
+{"hash-size",	'a', POPT_ARG_LONG, &(hash_size),0,0,0}
 
 #define STD_OPTIONS(stdout)					\
 {"version",		'V', POPT_ARG_NONE,0,OVERSION,0,0},	\
 {"verbose",		'v', POPT_ARG_NONE,0,OVERBOSE,0,0},	\
-{"to-stdout",		'c', POPT_ARG_NONE,&stdout, 0,0,0},	\
+{"to-stdout",		'c', POPT_ARG_NONE,&(stdout), 0,0,0},	\
 {"bzip2-compress",	'j', POPT_ARG_NONE,0, OBZIP2,0,0},	\
 {"gzip-compress",	'z', POPT_ARG_NONE,0, OGZIP,0,0}
 
 #define FORMAT_OPTIONS(long, short, string)		\
 {long, short, POPT_ARG_STRING, &string, 0,0,0}
 #define MD5_OPTION(md5var)				\
-{"ignore-md5",	'm',POPT_ARG_NONE, &md5var,0,0,0}
+{"ignore-md5",	'm',POPT_ARG_NONE, &(md5var),0,0,0}
 
 void usage(poptContext p_opt, int exitcode, const char *error, 
     const char *addl);
