7fc5206690e3f08b9d725af84e1d1dde602d99e4
bharring <bharring>
2003-10-09 11:00:48 +0000

    Friends don't let friends use BBEdit. Still cleaning up formatting fuck up's from it (tab's vs 4 spaces), and I only used it for like 2-3 weeks back at the start of this project... Either way, fixing formatting, commiting it so have a *Good* formatted version of this file, about to start conversions to new dcbuffer.{c,h} api.

--- diff-algs.c
+++ diff-algs.c
@@ -153,177 +153,177 @@ OneHalfPassCorrecting(CommandBuffer *buffer, RefHash *rhash, cfile *ver_cfh)
 {
     unsigned long ver_len, ref_len;
     unsigned long x, index, len;
-	unsigned long no_match=0, bad_match=0, good_match=0;
+    unsigned long no_match=0, bad_match=0, good_match=0;
     unsigned long vc, va, vs, vm, rm;
     unsigned int const rbuff_size = 4096, vbuff_size = 4096;
     unsigned char rbuff[rbuff_size], vbuff[vbuff_size];
     unsigned long rbuff_start=0, vbuff_start=0, rbuff_end=0, vbuff_end=0;
     ADLER32_SEED_CTX ads;
 
-	init_adler32_seed(&ads, rhash->seed_len, 1);
-	ref_len = cfile_len(rhash->ref_cfh);    
+    init_adler32_seed(&ads, rhash->seed_len, 1);
+    ref_len = cfile_len(rhash->ref_cfh);    
     ver_len = cfile_len(ver_cfh);
     
     va=vs =vc =0;
     vbuff_start = cseek(ver_cfh, 0, CSEEK_FSTART);
     //vbuff_start=0;
     vbuff_end=cread(ver_cfh, vbuff, MIN(vbuff_size, ver_len));
-	v3printf("vbuff_start(%lu), vbuff_end(%lu)\n", vbuff_start, vbuff_end);
-	while(vc + rhash->seed_len < ver_len) {
-		if(vc + rhash->seed_len > vbuff_start + vbuff_end) {
-			v3printf("full refresh of vbuff at vbuff_start(%lu), vc(%lu), fstart(%lu), abs(%lu)\n", 
-				vbuff_start, vc, 
-				ctell(ver_cfh, CSEEK_FSTART),
-				ctell(ver_cfh, CSEEK_ABS));
-			//if(vc > vbuff_start + vbuff_end) {
-				vbuff_start = cseek(ver_cfh, vc, CSEEK_FSTART);
-				vbuff_end = cread(ver_cfh, vbuff, MIN(ver_len - vbuff_start,vbuff_size));
-				v3printf("setting vbuff_start(%lu), vbuff_end(%lu), fstart(%lu)\n", 
-				vbuff_start, vbuff_end, ctell(ver_cfh, CSEEK_FSTART));
-			/*} else {
-				x = vbuff_size - (vc - vbuff_start);
-				memmove(vbuff, vbuff + vbuff_size -x, x);
-			}*/	
-		} else if (vc < vbuff_start) {
-			v3printf("partial refresh of vbuff at vbuff_start(%lu), vc(%lu), fstart(%lu), abs(%lu)\n", 
-				vbuff_start, vc,
-				ctell(ver_cfh, CSEEK_FSTART),
-				ctell(ver_cfh, CSEEK_ABS));
-			vbuff_start = cseek(ver_cfh, vc, CSEEK_FSTART);
-			vbuff_end = cread(ver_cfh, vbuff, MIN(ver_len - vbuff_start, vbuff_size));
-			v3printf("setting vbuff_start(%lu), vbuff_end(%lu), fstart(%lu)\n", 
-				vbuff_start, vbuff_end, ctell(ver_cfh, CSEEK_FSTART));
-		}
-		//assert(ctell(ver_cfh, CSEEK_FSTART)==vbuff_start + vbuff_end);
-		if(va -vc >= rhash->seed_len) {
-			update_adler32_seed(&ads, vbuff + vc - vbuff_start, rhash->seed_len);
-		} else {
-			update_adler32_seed(&ads, vbuff + (va - vbuff_start), vc + rhash->seed_len -va);
-		}
-		va = vc + rhash->seed_len;
-		index = hash_it(ads, rhash->hr_size);
-		if(rhash->hash[index]>0) {	
-			if(rhash->hash[index] != cseek(rhash->ref_cfh, rhash->hash[index], CSEEK_FSTART)) {
-				perror("cseek error for ref\n");
-				v3printf("ctell(%lu), wanted(%lu)\n", ctell(rhash->ref_cfh, CSEEK_FSTART), 
-					rhash->hash[index]);
-				abort();
-			} else {
-				rbuff_start = rhash->hash[index];
-				rbuff_end = cread(rhash->ref_cfh, rbuff, rbuff_size);
-			}	
-			if(memcmp(rbuff, vbuff+vc - vbuff_start, rhash->seed_len)!=0){
-				bad_match++;
-				vc++;
-				continue;
-		    }
-		    good_match++;
-		    x=0;
-		    vm = vc;
-		    rm = rhash->hash[index];
-		    /*backwards matching*/
-		    if(vm-vbuff_start==0) {
-		   		vbuff_start=
-		   			cseek(ver_cfh, 
-		   			(vbuff_size > vbuff_start ? 0 : vbuff_start - vbuff_size),
-		   			CSEEK_FSTART);
-		   		vbuff_end=cread(ver_cfh, vbuff, vbuff_size);
-		   		v3printf("back match moved vbuff to start(%lu), end(%lu)\n",
-		    			vbuff_start, vbuff_end);
-		   	}
-		   	if(rm-rbuff_start==0) {
-		   		rbuff_start=
-		  			cseek(rhash->ref_cfh, 
-		  			(rbuff_size > rbuff_start ? 0 : rbuff_start - rbuff_size),
-		  			CSEEK_FSTART);
-		  		rbuff_end=cread(rhash->ref_cfh, rbuff, rbuff_size);
-		  		v3printf("back match moved rbuff to start(%lu), end(%lu)\n",
-		    			rbuff_start, rbuff_end);
-			}
-		    while(vm > 0 && rm > 0 && 
-		    	vbuff[vm -vbuff_start-1]==rbuff[rm -rbuff_start -1]) {
-				vm--;
-				rm--;
-		    	if(vm-vbuff_start==0) {
-		    		vbuff_start=
-		    			cseek(ver_cfh, (vbuff_size > vbuff_start ? 0 :
-		    			vbuff_start - vbuff_size), CSEEK_FSTART);
-		    		vbuff_end=cread(ver_cfh, vbuff, vbuff_size);
-		    		v3printf("back match moved vbuff to start(%lu), end(%lu)\n",
-		    			vbuff_start, vbuff_end);
-		    	}
-		    	if(rm-rbuff_start==0) {
-		    		rbuff_start=
-		    			cseek(rhash->ref_cfh, (rbuff_size > rbuff_start ? 0 :
-		    			rbuff_start - rbuff_size), CSEEK_FSTART);
-		    		rbuff_end=cread(rhash->ref_cfh, rbuff, rbuff_size);
-		    		v3printf("back match moved rbuff to start(%lu), end(%lu)\n",
-		    			rbuff_start, rbuff_end);
-		    	}
-		    }
-		    len=(vc -vm) + rhash->seed_len;
+    v3printf("vbuff_start(%lu), vbuff_end(%lu)\n", vbuff_start, vbuff_end);
+    while(vc + rhash->seed_len < ver_len) {
+	if(vc + rhash->seed_len > vbuff_start + vbuff_end) {
+	    v3printf("full refresh of vbuff at vbuff_start(%lu), vc(%lu), fstart(%lu), abs(%lu)\n", 
+		vbuff_start, vc, ctell(ver_cfh, CSEEK_FSTART),
+		ctell(ver_cfh, CSEEK_ABS));
+	    //if(vc > vbuff_start + vbuff_end) {
+		vbuff_start = cseek(ver_cfh, vc, CSEEK_FSTART);
+		vbuff_end = cread(ver_cfh, vbuff, MIN(ver_len - vbuff_start,vbuff_size));
+		v3printf("setting vbuff_start(%lu), vbuff_end(%lu), fstart(%lu)\n", 
+		vbuff_start, vbuff_end, ctell(ver_cfh, CSEEK_FSTART));
+	    /*} else {
+		x = vbuff_size - (vc - vbuff_start);
+		memmove(vbuff, vbuff + vbuff_size -x, x);
+	    }*/	
+	} else if (vc < vbuff_start) {
+	    v3printf("partial refresh of vbuff at vbuff_start(%lu), vc(%lu), fstart(%lu), abs(%lu)\n", 
+		vbuff_start, vc,ctell(ver_cfh, CSEEK_FSTART),
+		    ctell(ver_cfh, CSEEK_ABS));
+	    vbuff_start = cseek(ver_cfh, vc, CSEEK_FSTART);
+	    vbuff_end = cread(ver_cfh, vbuff, MIN(ver_len - vbuff_start, vbuff_size));
+	    v3printf("setting vbuff_start(%lu), vbuff_end(%lu), fstart(%lu)\n", 
+		vbuff_start, vbuff_end, ctell(ver_cfh, CSEEK_FSTART));
+	}
+	//assert(ctell(ver_cfh, CSEEK_FSTART)==vbuff_start + vbuff_end);
+	if(va -vc >= rhash->seed_len) {
+	    update_adler32_seed(&ads, vbuff + vc - vbuff_start, rhash->seed_len);
+	} else {
+	    update_adler32_seed(&ads, vbuff + (va - vbuff_start), vc + rhash->seed_len -va);
+	}
+	va = vc + rhash->seed_len;
+	index = hash_it(ads, rhash->hr_size);
+	if(rhash->hash[index]>0) {	
+	    if(rhash->hash[index] != cseek(rhash->ref_cfh, 
+		rhash->hash[index], CSEEK_FSTART)) {
 
-		    if(vm + len >= vbuff_start + vbuff_size) {
-		    	vbuff_start=cseek(ver_cfh, vm+len , CSEEK_FSTART);
-		    	vbuff_end=cread(ver_cfh, vbuff,
-		    		MIN(vbuff_size, ver_len - vbuff_start));
-		    	v3printf("forw match moved vbuff to start(%lu), end(%lu)\n",
-		    		vbuff_start, vbuff_end);
-		    }
-		    if( rm + len >= rbuff_start + rbuff_size) {
-		    	rbuff_start=cseek(rhash->ref_cfh, rm + len, CSEEK_FSTART);
-		    	rbuff_end=cread(rhash->ref_cfh, rbuff,
-		    		MIN(rbuff_size, ref_len - rbuff_start));
-		    		v3printf("forw match moved rbuff to start(%lu), end(%lu)\n",
-		    			rbuff_start, rbuff_end);
-		    }
-		    while(rm + len < ref_len && vm + len < ver_len &&
-		    	rbuff[rm + len - rbuff_start]
-		    	==vbuff[vm + len - vbuff_start]) {
-		    	len++;
-		    	if(vm + len -vbuff_start==vbuff_size) {
-		    		vbuff_start += vbuff_end;
-		    		vbuff_end=cread(ver_cfh, vbuff,
-		    			MIN(vbuff_size, ver_len -vbuff_start));
-		    		v3printf("forw match moved rbuff to start(%lu), end(%lu)\n",
-		    			vbuff_start, vbuff_end);
-		    	}
-		    	if(rm + len -rbuff_start==rbuff_size) {
-		    		rbuff_start += rbuff_end;
-		    		rbuff_end=cread(rhash->ref_cfh, rbuff,
-		    			MIN(rbuff_size, ref_len -rbuff_start));
-		    		v3printf("forw match moved rbuff to start(%lu), end(%lu)\n",
-		    			rbuff_start, rbuff_end);
-		    	}
-		    }
-
-	    	if (vs <= vm) {
-				if (vs < vm) {
-		    		v2printf("    adding vstart(%lu), len(%lu), vend(%lu): (vs < vm)\n",
-						cfile_start_offset(ver_cfh) + vs, vm-vs, vm);
-		    		DCBufferAddCmd(buffer, DC_ADD, cfile_start_offset(ver_cfh) + vs, vm - vs);
-				}
-				v2printf("    copying offset(%lu), len(%lu)\n", 
-					cfile_start_offset(ver_cfh) + vm, len);
-				DCBufferAddCmd(buffer, DC_COPY, cfile_start_offset(rhash->ref_cfh) + rm, len);
-		    } else {
-		    		v2printf("    adding vstart(%lu)\n",
-			cfile_start_offset(ver_cfh) + vs);
-				v2printf("    truncating(%lu) bytes: (vm < vs)\n", vs - vm);
-				DCBufferTruncate(buffer, vs - vm);
-				v2printf("    replacement copy: offset(%lu), len(%lu)\n", 
-					cfile_start_offset(rhash->ref_cfh) + rm, len);
-				DCBufferAddCmd(buffer, DC_COPY, cfile_start_offset(rhash->ref_cfh) + rm, len);
-	    	}
-	    	vs = vm + len;
-	    	vc = vs -1;
-		} else {
-	    	no_match++;
-		}
+		perror("cseek error for ref\n");
+		v3printf("ctell(%lu), wanted(%lu)\n", ctell(rhash->ref_cfh, CSEEK_FSTART), 
+		   rhash->hash[index]);
+		abort();
+	    } else {
+		rbuff_start = rhash->hash[index];
+		rbuff_end = cread(rhash->ref_cfh, rbuff, rbuff_size);
+	    }	
+	    if(memcmp(rbuff, vbuff+vc - vbuff_start, rhash->seed_len)!=0){
+		bad_match++;
 		vc++;
+		continue;
+	    }
+	    good_match++;
+	    x=0;
+	    vm = vc;
+	    rm = rhash->hash[index];
+	    /*backwards matching*/
+	    if(vm-vbuff_start==0) {
+		vbuff_start= cseek(ver_cfh, (vbuff_size > vbuff_start ? 0 : 
+		    vbuff_start - vbuff_size), CSEEK_FSTART);
+		vbuff_end=cread(ver_cfh, vbuff, vbuff_size);
+		v3printf("back match moved vbuff to start(%lu), end(%lu)\n",
+		    vbuff_start, vbuff_end);
+	    }
+	    if(rm-rbuff_start==0) {
+		rbuff_start= cseek(rhash->ref_cfh, (rbuff_size > 
+		    rbuff_start ? 0 : rbuff_start - rbuff_size), CSEEK_FSTART);
+		rbuff_end=cread(rhash->ref_cfh, rbuff, rbuff_size);
+		v3printf("back match moved rbuff to start(%lu), end(%lu)\n",
+		    rbuff_start, rbuff_end);
+	    }
+	    while(vm > 0 && rm > 0 && vbuff[vm -vbuff_start-1] == 
+		rbuff[rm -rbuff_start -1]) {
+		vm--;
+		rm--;
+		if(vm-vbuff_start==0) {
+		    vbuff_start= cseek(ver_cfh, (vbuff_size > 
+			vbuff_start ? 0 : vbuff_start - vbuff_size), 
+			CSEEK_FSTART);
+		    vbuff_end=cread(ver_cfh, vbuff, vbuff_size);
+		    v3printf("back match moved vbuff to start(%lu), end(%lu)\n",
+			vbuff_start, vbuff_end);
+		}
+		if(rm-rbuff_start==0) {
+		    rbuff_start= cseek(rhash->ref_cfh, (rbuff_size > 
+			rbuff_start ? 0 : rbuff_start - rbuff_size), 
+			    CSEEK_FSTART);
+		    rbuff_end=cread(rhash->ref_cfh, rbuff, rbuff_size);
+		    v3printf("back match moved rbuff to start(%lu), end(%lu)\n",
+		        rbuff_start, rbuff_end);
+		}
+	    }
+	    len=(vc -vm) + rhash->seed_len;
+
+	    if(vm + len >= vbuff_start + vbuff_size) {
+		vbuff_start=cseek(ver_cfh, vm+len , CSEEK_FSTART);
+		vbuff_end=cread(ver_cfh, vbuff, MIN(vbuff_size, ver_len - 
+		    vbuff_start));
+		v3printf("forw match moved vbuff to start(%lu), end(%lu)\n",
+		    vbuff_start, vbuff_end);
+	    }
+	    if(rm + len >= rbuff_start + rbuff_size) {
+		rbuff_start=cseek(rhash->ref_cfh, rm + len, CSEEK_FSTART);
+	    	rbuff_end=cread(rhash->ref_cfh, rbuff, MIN(rbuff_size, 
+		    ref_len - rbuff_start));
+		v3printf("forw match moved rbuff to start(%lu), end(%lu)\n",
+		    rbuff_start, rbuff_end);
+	    }
+	    while(rm + len < ref_len && vm + len < ver_len &&
+		rbuff[rm + len - rbuff_start] == vbuff[vm + len - 
+		vbuff_start]) {
+		len++;
+		if(vm + len -vbuff_start==vbuff_size) {
+		    vbuff_start += vbuff_end;
+		    vbuff_end=cread(ver_cfh, vbuff, MIN(vbuff_size, ver_len - 
+			vbuff_start));
+		    	v3printf("forw match moved rbuff to start(%lu), end(%lu)\n",
+		    	    vbuff_start, vbuff_end);
+		}
+		if(rm + len -rbuff_start==rbuff_size) {
+		    rbuff_start += rbuff_end;
+		    rbuff_end=cread(rhash->ref_cfh, rbuff, MIN(rbuff_size,
+			ref_len -rbuff_start));
+		    v3printf("forw match moved rbuff to start(%lu), end(%lu)\n",
+		    	rbuff_start, rbuff_end);
+		}
+	    }
+
+	    if (vs <= vm) {
+		if (vs < vm) {
+		    v2printf("\tadding vstart(%lu), len(%lu), vend(%lu): (vs < vm)\n",
+			cfile_start_offset(ver_cfh) + vs, vm-vs, vm);
+		    DCBufferAddCmd(buffer, DC_ADD, 
+			cfile_start_offset(ver_cfh) + vs, vm - vs);
+		}
+		v2printf("    copying offset(%lu), len(%lu)\n", 
+		    cfile_start_offset(ver_cfh) + vm, len);
+		DCBufferAddCmd(buffer, DC_COPY, 
+		    cfile_start_offset(rhash->ref_cfh) + rm, len);
+	    } else {
+		v2printf("    adding vstart(%lu)\n",
+		    cfile_start_offset(ver_cfh) + vs);
+		v2printf("    truncating(%lu) bytes: (vm < vs)\n", vs - vm);
+		DCBufferTruncate(buffer, vs - vm);
+		v2printf("    replacement copy: offset(%lu), len(%lu)\n", 
+		    cfile_start_offset(rhash->ref_cfh) + rm, len);
+		DCBufferAddCmd(buffer, DC_COPY, 
+		    cfile_start_offset(rhash->ref_cfh) + rm, len);
+	    }
+	    vs = vm + len;
+	    vc = vs -1;
+	} else {
+	    no_match++;
+	}
+	vc++;
     }
     if (vs != ver_len) {
-    	DCBufferAddCmd(buffer, DC_ADD, cfile_start_offset(ver_cfh) + vs, ver_len - vs);
+    	DCBufferAddCmd(buffer, DC_ADD, cfile_start_offset(ver_cfh) + vs, 
+	    ver_len - vs);
     }
     free_adler32_seed(&ads);
     return 0;
