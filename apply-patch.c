2a7c33c5df64d4aabec103024c0e7d87936cc6b6
bharring <bharring>
2003-12-21 12:21:28 +0000

    Adjustments to use new cfile/dcbuffer multiple possible add_srcs.  function ptrs are fun...

--- apply-patch.c
+++ apply-patch.c
@@ -23,7 +23,7 @@
 #include "defs.h"
 
 void 
-reconstructFile(CommandBuffer *dcbuff, cfile *src_cfh, cfile *out_cfh)
+reconstructFile(CommandBuffer *dcbuff, cfile *out_cfh)
 {
     DCommand dc;
     assert(DCBUFFER_FULL_TYPE == dcbuff->DCBtype);
@@ -34,15 +34,19 @@ reconstructFile(CommandBuffer *dcbuff, cfile *src_cfh, cfile *out_cfh)
 	    v2printf("copy command, offset(%lu), len(%lu)\n",
 		dc.loc.offset, dc.loc.len);
 		//cseek(src_cfh, DCBF_cur_off(dcbuff), CSEEK_FSTART);
-	    if(dc.loc.len != copy_cfile_block(out_cfh, src_cfh, dc.loc.offset,
-		dc.loc.len))
+//	    if(dc.loc.len != copy_cfile_block(out_cfh, src_cfh, dc.loc.offset,
+//		dc.loc.len))
+	    if(dc.loc.len != copyDCB_copy_src(dcbuff, &dc, out_cfh)) {
 		abort();
+	    }
 	} else {
 	    v2printf("add command, offset(%lu), len(%lu)\n", 
 		dc.loc.offset, dc.loc.len);
-	    if(dc.loc.len != copy_cfile_block(out_cfh, dcbuff->add_cfh, 
-		dc.loc.offset, dc.loc.len))
+//	    if(dc.loc.len != copy_cfile_block(out_cfh, dcbuff->add_cfh, 
+//		dc.loc.offset, dc.loc.len))
+	    if(dc.loc.len != copyDCB_add_src(dcbuff, &dc, out_cfh)) {
 		abort();
+	    }
 	}
     }
 }
--- apply-patch.h
+++ apply-patch.h
@@ -18,6 +18,6 @@
 #ifndef _HEADER_APPLY_PATCH
 #define _HEADER_APPLY_PATCH 1
 
-void reconstructFile(CommandBuffer *dcbuff, cfile *src_cfh, cfile *out_cfh);
+void reconstructFile(CommandBuffer *dcbuff, cfile *out_cfh);
 
 #endif 
