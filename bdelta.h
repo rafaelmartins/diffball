bd46c0b5d2bb20185488bd7092c5b2387a681270
bharring <bharring>
2004-01-25 12:08:33 +0000

    Added info regarding bdelta's breakage.

--- bdelta.h
+++ bdelta.h
@@ -32,4 +32,22 @@ signed int bdeltaReconstructDCBuff(cfile *patchf,
 	CommandBuffer *dcbuff);
 
 
+/* BDELTA Notes
+  command is 3 intsize (intsize locked to 4 in last version of bdelta)
+  copy offset
+  add len
+  copy len
+
+  Note, bdelta-0.1 *does not abide by it's own format*.
+  If size2 != last match, it incorrectly outputs the last command as a copy 
+  len, rather then an add.  It is an add command however.
+
+  That one sucked to find/fix.  On the plus side, bdelta is consistent about 
+  that screw up in it's patch generation, and reconstruction, so it's not a 
+  prob for it.
+
+  This is only a prob for reconstruct, encode will be written to always 
+  output the full command buffer (it doesn't leave off trailing nulls).
+*/
+
 #endif
