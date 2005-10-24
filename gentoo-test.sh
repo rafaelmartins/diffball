406cd624138b2f2a6f33344d90b118bcb3ea9920
bharring <bharring>
2005-10-24 11:24:14 +0000

    include release tag's into ChangeLog, and a quick change to my local testing script

--- gentoo-test.sh
+++ gentoo-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo ls && make dist && \
+sudo true && make dist && \
 cp diffball-0.7.1.tar.bz2 /usr/portage/distfiles && \
 rm -f /usr/local/portage/dev-util/diffball/files/digest-diffball-0.7.1 && \
 ebuild /usr/local/portage/dev-util/diffball/diffball-0.7.1.ebuild digest && \
