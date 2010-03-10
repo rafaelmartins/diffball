6ac73f5746017138cbea9cf2693e67dad268123b
Brian Harring <ferringb@gmail.com>
2010-03-10 14:44:39 -0800

    update to automake 1.10...

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 autoheader || { echo "failed autoheader"; exit 1; };
-aclocal-1.8 || { echo "failed aclocal"; exit 1; };
+aclocal-1.10 || { echo "failed aclocal"; exit 1; };
 libtoolize --automake -c -f || { echo "failed libtoolize"; exit 1; }
 autoconf || { echo "failed autoconf"; exit 1; }
 touch ChangeLog
-automake-1.8 -a -c || { "echo failed automake"; exit 1; }
+automake-1.10 -a -c || { "echo failed automake"; exit 1; }
 
 if [ -x ./test.sh ] ; then
 	exec ./test.sh "$@"
