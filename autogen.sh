6c3bf03829ce592a306c7930ae172efa37181c29
Brian Harring <ferringb@gmail.com>
2010-03-14 23:23:08 -0700

    remove pointless ChangeLog touching

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,6 @@ aclocal-1.10 || { echo "failed aclocal"; exit 1; };
 libtoolize --automake -c -f || { echo "failed libtoolize"; exit 1; }
 autoconf || { echo "failed autoconf"; exit 1; }
 autoheader || { echo "failed autoheader"; exit ; }
-touch ChangeLog
 automake-1.10 -a -c || { "echo failed automake"; exit 1; }
 
 if [ -x ./test.sh ] ; then
